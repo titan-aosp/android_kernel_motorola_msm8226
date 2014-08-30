de891822b7226c4f0e2cda4595e0f860dbd0b15a
Alberto97 <albertop2197@gmail.com>
2014-08-30 22:45:06 +0200

    doubletap2wake: Updates
    -Define dt2w Area
    -Add option to enable dt2w only on bottom of screen
       1 - Bottom of screen
       2 - Fullscreen
       0 - Disabled
    
    Thanks to @flar2
    
    Signed-off-by: Alberto97 <albertop2197@gmail.com>

--- drivers/input/touchscreen/doubletap2wake.c
+++ drivers/input/touchscreen/doubletap2wake.c
@@ -81,6 +81,9 @@ static int __init read_dt2w_cmdline(char *dt2w)
 	if (strcmp(dt2w, "1") == 0) {
 		pr_info("[cmdline_dt2w]: DoubleTap2Wake enabled. | dt2w='%s'\n", dt2w);
 		dt2w_switch = 1;
+	} else if (strcmp(dt2w, "2") == 0) {
+		pr_info("[cmdline_dt2w]: DoubleTap2Wake fullscreen enabled. | dt2w='%s'\n", dt2w);
+		dt2w_switch = 2;
 	} else if (strcmp(dt2w, "0") == 0) {
 		pr_info("[cmdline_dt2w]: DoubleTap2Wake disabled. | dt2w='%s'\n", dt2w);
 		dt2w_switch = 0;
@@ -146,6 +149,13 @@ static void detect_doubletap2wake(int x, int y, bool st)
         pr_info(LOGTAG"x,y(%4d,%4d) single:%s\n",
                 x, y, (single_touch) ? "true" : "false");
 #endif
+
+	if (x < 50 || x > 690)
+         	return;
+
+	if (dt2w_switch < 2 && y < 640)
+        	return;
+
 	if ((single_touch) && (dt2w_switch > 0) && (exec_count) && (touch_cnt)) {
 		touch_cnt = false;
 		if (touch_nr == 0) {
